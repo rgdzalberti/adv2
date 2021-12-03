6c9b3e6c0633c73e3ec64fc053ed65988dbaae16
Ricardo <rgallego@iesrafaelalberti.es>
2021-12-03 18:31:16 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
+import java.io.File
+
+val movesetList = mutableListOf<String>()
+val upList = mutableListOf<String>()
+val downList = mutableListOf<String>()
+val forwardList = mutableListOf<String>()
+
+
 fun main() {
 
-    
+    var file: File = File("C:\\Users\\usuarioT\\Desktop\\advent\\input.txt")
+
+    file.forEachLine { movesetList.add(it)}
+
+    movesetList.forEach {if (it.contains("up")) {upList.add(it)} }
+    movesetList.forEach {if (it.contains("down")) {downList.add(it)} }
+    movesetList.forEach {if (it.contains("forward")) {forwardList.add(it)} }
+
+    var upDelimiter = "up"
+    var downDelimiter = "down"
+    var forwardDelimiter = "forward"
+
+    val upNumbers = upList.split(upDelimiter)
+    //val upNumbers = upList.forEach {split(upDelimiter)}
 
 }
