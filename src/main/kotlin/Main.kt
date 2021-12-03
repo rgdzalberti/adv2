610ab7ed96d04565631d4ee5f59d9ccb5d4a4dc3
Ricardo <rgallego@iesrafaelalberti.es>
2021-12-03 19:16:44 +0100

    parte 1 ole

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,11 +16,30 @@ fun main() {
     movesetList.forEach {if (it.contains("down")) {downList.add(it)} }
     movesetList.forEach {if (it.contains("forward")) {forwardList.add(it)} }
 
-    var upDelimiter = "up"
-    var downDelimiter = "down"
-    var forwardDelimiter = "forward"
 
-    val upNumbers = upList.split(upDelimiter)
-    //val upNumbers = upList.forEach {split(upDelimiter)}
+    val upNumbers = mutableListOf<Int>()
+    val downNumbers = mutableListOf<Int>()
+    val forwardNumbers = mutableListOf<Int>()
+
+    upList.forEach {upNumbers.add(it.removeRange(0,3).toInt())}
+    downList.forEach {downNumbers.add(it.removeRange(0,5).toInt())}
+    forwardList.forEach {forwardNumbers.add(it.removeRange(0,8).toInt())}
+
+    var horizontal: Int = 0
+    var depth:  Int = 0
+
+    var down:  Int = 0
+    var up: Int = 0
+
+    for (i in 0 until forwardNumbers.size) {horizontal = horizontal + forwardNumbers[i]}
+    for (i in 0 until upNumbers.size) {up = up + upNumbers[i]}
+    for (i in 0 until downNumbers.size) {down = down + downNumbers[i]}
+
+    depth = down - up
+
+    var result = horizontal * depth
+    println(result)
+
+
 
 }
